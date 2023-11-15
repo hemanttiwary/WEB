4c75b17e012ab2188cfb524e7cca1a3192799904
hemanttiwary <hemant.appu@gmail.com>
2023-11-15 12:31:22 +0530

    New HTML Files updated

---
+++ script.js
@@ -0,0 +1,24 @@
+let hr = document.getElementById('hour');
+let mint = document.getElementById('min');
+let secon = document.getElementById('sec');
+
+function displayTime(){
+	let date = new Date();
+
+	let hh = date.getHours();
+	let mm = date.getMinutes();
+	let ss = date.getSeconds();
+
+	let hrr = 30*hh + mm/2;
+	let minrot = 6*mm;
+	let secrot = 6*ss;  
+
+	hr.style.transform = `rotate(${hrr}deg)`;
+	mint.style.transform = `rotate(${minrot}deg)`;
+	secon.style.transform = `rotate(${secrot}deg)`;
+	
+}
+
+setInterval(displayTime, 1000);
+
+
